4d21101bb2eaa65fc21f65f8b4966aa6f2dbd016
Morten la Cour <lacour@gmail.com>
2021-04-23 09:38:58 +0200

    EuroV songs working raw

--- EveryGraph/GraphQL/Base/EveryGraphQuery.cs
+++ EveryGraph/GraphQL/Base/EveryGraphQuery.cs
@@ -1,12 +1,27 @@
-﻿using GraphQL.Types;
+﻿using EveryGraph.GraphQL.Types;
+using GraphQL.Types;
+using KaggleReader.Library.Services;
 
 namespace EveryGraph.GraphQL.Base
 {
     public class EveryGraphQuery : ObjectGraphType
     {
-        public EveryGraphQuery()
+        public EveryGraphQuery(IEurovisionHandler eurovisionHandler)
         {
             Field<StringGraphType>("version", resolve: _ => $"0.0.1");
+
+
+
+            #region Eurovision Songs
+            Field<ListGraphType<EurovisionLyricGraphType>>(
+                    "eurovisionSongs",
+                    arguments: new QueryArguments { },
+                    resolve: context => {
+                        return eurovisionHandler
+                                    .GetLyricsAsync(
+                                            cancellationToken: context.CancellationToken);
+                    });
+            #endregion
         }
     }
 }
---
+++ EveryGraph/GraphQL/Types/EurovisionLyricGraphType.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Types;
+using KaggleReader.Library.Models.Eurovision;
+
+namespace EveryGraph.GraphQL.Types
+{
+    public class EurovisionLyricGraphType : ObjectGraphType<EuroVisionLyricModel>
+    {
+        public EurovisionLyricGraphType()
+        {
+            Field(f => f.Artist);
+        }
+    }
+}
